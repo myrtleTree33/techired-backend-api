753cbef2d9252884ffd3a2078decace9b340a857
jhtong <me@joeltong.org>
2019-03-23 23:54:16 +0800

    added ability to search by country, city, number of yrs exp

--- src/routes/supersearchRoutes.js
+++ src/routes/supersearchRoutes.js
@@ -1,16 +1,91 @@
 import { Router } from 'express';
+import moment from 'moment';
+
 import { ensureAuth } from '../utils/socialAuth';
 import Profile from '../models/Profile';
 import Repo from '../models/Repo';
 
 const routes = Router();
 
+function addLocationFilter(location) {
+  if (!location) {
+    return {};
+  }
+
+  return {
+    $or: [
+      {
+        location: { $regex: `${location}`, $options: 'i' }
+      },
+      {
+        countries: { $elemMatch: { $regex: `${location}`, $options: 'i' } }
+      },
+      {
+        cities: { $elemMatch: { $regex: `${location}`, $options: 'i' } }
+      }
+    ]
+  };
+}
+
+function addCities(cities) {
+  if (cities.length === 0) {
+    return {};
+  }
+
+  const cities2 = cities.map(c => c.trim().toLowerCase());
+
+  return {
+    cities: {
+      $in: cities2
+    }
+  };
+}
+
+function addCountries(countries) {
+  if (countries.length === 0) {
+    return {};
+  }
+
+  const countries2 = countries.map(c => c.trim().toLowerCase());
+
+  return {
+    countries: {
+      $in: countries2
+    }
+  };
+}
+
+function addEarliestLangs(ownedReposLangs) {
+  if (Object.keys(ownedReposLangs).length === 0) {
+    return {};
+  }
+
+  const keys = Object.keys(ownedReposLangs);
+  const query = keys.map(k => {
+    const numMonths = ownedReposLangs[k];
+    return {
+      [`ownedReposLangsEarliest.${k}`]: {
+        $lte: moment()
+          .subtract(numMonths, 'months')
+          .toDate()
+      }
+    };
+  });
+
+  console.log(query);
+
+  return {
+    $or: query
+  };
+}
+
 /**
  * Meta search
  */
-routes.get('/', async (req, res, next) => {
+routes.post('/', async (req, res, next) => {
   const PER_PAGE = parseInt(process.env.PER_PAGE, 10);
-  const { page, location } = req.query;
+  const { page, location, cities = [], countries = [], ownedReposLangsMonths = {} } = req.body;
+  console.log(page, location, cities);
   const pageInt = parseInt(page || 1, 10);
   const pagination = {
     limit: PER_PAGE, // max 20
@@ -18,10 +93,18 @@ routes.get('/', async (req, res, next) => {
   };
 
   const profiles = await Profile.find({
-    location: { $regex: `${location}`, $options: 'i' }
+    $and: [
+      addLocationFilter(location),
+      addCities(cities),
+      addCountries(countries),
+      addEarliestLangs(ownedReposLangsMonths)
+    ]
   })
+    .sort({ _id: -1 })
     .limit(pagination.limit)
     .skip(pagination.skip);
+
+  console.log(`${profiles.length} profiles found.`);
   res.json(profiles);
 });
 
